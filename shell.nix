c13a03a17c0f82f54c53a9aa5f2c0398338bdfc8
Jaka Hudoklin <jaka@x-truder.net>
2022-01-11 18:55:38 +0000

    dev: no need to use flakes for now

--- shell.nix
+++ shell.nix
@@ -1,3 +1,18 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = ./.;
-}).shellNix
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.11.tar.gz") { } }:
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    # needed by nix
+    nixpkgs-fmt
+    rnix-lsp
+
+    # for building docker images
+    docker-client
+    gnumake
+    git
+
+    # go dev
+    go
+  ];
+}
