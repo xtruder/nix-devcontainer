020ac7fd91e762d61c8784e9a076b6f9a91c69c8
Jaka Hudoklin <jaka@x-truder.net>
2022-08-30 12:06:14 +0000

    Fix /tmp permissions for codespaces
    vscode codespaces set default permissions on /tmp. These will
    produce invalid permissions on files built with nix. This fix
    removes default permissions set on /tmp
    
    Fixes #12
    Closes #13

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# github codespaces fixes
+if [ "${CODESPACES}" = true ]; then
+  # vscode codespaces set default permissions on /tmp. These will
+  # produce invalid permissions on files built with nix. This fix
+  # removes default permissions set on /tmp
+  sudo setfacl --remove-default /tmp
+fi
+
 if [ ! -z "${PRELOAD_EXTENSIONS}" ]; then
   ext-preloader &
 fi
